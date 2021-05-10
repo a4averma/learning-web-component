3b2f35697b4662f5d66e0691a66f2bf09195def7
Aditya Verma <siradityaverma@gmail.com>
2021-05-10 13:25:20 +0530

    Finish todo

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,42 +1,88 @@
-import {LitElement, html} from 'lit';
-import {customElement, property, query} from 'lit/decorators.js';
+import { LitElement, html, css } from "lit";
+import { customElement, property, query } from "lit/decorators.js";
 
-@customElement('my-element')
+type ToDoItem = {
+  text: string;
+  completed: boolean;
+};
+
+@customElement("my-element")
 class MyElement extends LitElement {
-  @property()
+  @property({ attribute: false })
   listItems = [
-    { text: 'Start Lit tutorial', completed: true },
-    { text: 'Make to-do list', completed: false }
+    { text: "Start Lit tutorial", completed: true },
+    { text: "Make to-do list", completed: false },
   ];
-  
-  @query('#newitem')
-  input!: HTMLInputElement;
-  
-  addToDo() {
-   this.listItems.push({text: this.input.value, completed: false});
-   this.requestUpdate();
-   this.input.value = '';
-  }
 
-  render() {
-    return html`
-      <h2>To Do</h2>
-      <ul>
-         ${this.listItems.map((item) =>
-          html`<li>${item.text}</li>`
-         )}
-      </ul>
-      <input id="newitem" aria-label="New item">
-      <button @click=${this.addToDo}>Add</button>
+  static get styles() {
+    return css`
+      .completed {
+        text-decoration-line: line-through;
+        color: #777;
+      }
     `;
   }
 
+  @query("#newitem")
+  input!: HTMLInputElement;
+
+  addToDo() {
+    this.listItems.push({ text: this.input.value, completed: false });
+    this.requestUpdate();
+    this.input.value = "";
+  }
+
+  @property()
+  hideCompleted = false;
+
+  toggleCompleted(item: ToDoItem) {
+    item.completed = !item.completed;
+    this.requestUpdate();
+  }
+
+  setHideCompleted(e: Event) {
+    this.hideCompleted = (e.target as HTMLInputElement).checked;
+  }
+
+  render() {
+    const items = this.hideCompleted
+      ? this.listItems.filter((item) => !item.completed)
+      : this.listItems;
+    const caughtUpMessage = html` <p>You're all caught up!</p> `;
+    const todos = html`
+      <ul>
+        ${items.map(
+          (item) =>
+            html` <li
+              class=${item.completed ? "completed" : ""}
+              @click=${() => this.toggleCompleted(item)}
+            >
+              ${item.text}
+            </li>`
+        )}
+      </ul>
+    `;
+    const todosOrMessage = items.length > 0 ? todos : caughtUpMessage;
+    return html`
+      <h2>To Do</h2>
+      {todosOrMessage}
+      <input id="newitem" aria-label="New item" />
+      <button @click=${this.addToDo}>Add</button>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          @change=${this.setHideCompleted}
+          ?checked=${this.hideCompleted}
+        />
+        Hide completed
+      </label>
+    `;
+  }
 }
-
-
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement
+    "my-element": MyElement;
   }
 }
